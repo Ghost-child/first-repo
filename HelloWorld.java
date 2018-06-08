fef24e8417a969aeb8fb90c393779b49cb8f1aad
lennox <lennoxclaudio@gmail.com>
2018-06-08 12:03:47 +0300

    Add a method to say hi

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,13 @@
 public class HelloWorld{
     public static void main(String args[]){
         System.out.println("Hello World from Java!");
+
+        HelloWorld obj = new HelloWorld();
+        
+        obj.sayHello("Lennox");
+    }
+
+    public void sayHello(String name){
+        System.out.println("Hello there " + name);
     }
 }
